c36d5211196449cde191655c1eb0ea67f6cfa0bb
Yangtianhao <y3314708477@qq.com>
2025-05-12 17:29:56 +0800

    part1 矩阵类更改+RELU函数

--- part1/Matrix.cpp
+++ part1/Matrix.cpp
@@ -4,23 +4,27 @@
 
 
 //默认构造矩阵
- Matrix::Matrix(int r,int c)
+ Matrix::Matrix(size_t r,size_t c)
  {
     rows=r;
     colums=c;
-    elements.resize(r*c,0.0f);//resize方法用一维向量存储二维数据
+    elements.resize(rows*colums,0.0f);//resize方法用一维向量存储二维数据
  }
 
  //指定构造矩阵
  Matrix::Matrix(const std::vector<std::vector<float>> mat)
  {
+    if (mat.empty()) {
+        rows = colums = 0;
+        return;
+    }
     rows = mat.size();
     colums = mat[0].size();
-    elements.resize(rows,colums);
+    elements.resize(rows*colums);
     //循环嵌套结构给矩阵赋值
-    for (int i = 0; i <rows; ++i)
+    for (size_t i = 0; i <rows; ++i)
     {
-        for (int j = 0; j < colums; ++j)
+        for (size_t j = 0; j < colums; ++j)
         {
             elements[i*colums+j]=mat[i][j];//一维向量存储二维数据
         }
@@ -30,6 +34,19 @@
  Matrix::~Matrix()
 {
     //析构函数
+}
+
+//打印矩阵
+void Matrix::print() const
+{
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < colums; ++j)
+        {
+            std::cout << elements[i*colums+j] << "\t";
+        }
+        std::cout<<std::endl;
+    }
 }
 
 //矩阵加法
@@ -44,7 +61,7 @@ Matrix Matrix::operator+(const Matrix& other)
     Matrix result(rows,colums);
     
     //两个一维向量相加
-    for (int i = 0; i < elements.size(); ++i)
+    for (size_t i = 0; i < rows*colums; ++i)
     {
          result.elements[i] =  elements[i]+other.elements[i];
     }
@@ -52,20 +69,20 @@ Matrix Matrix::operator+(const Matrix& other)
 }
 
 //重构()进行矩阵索引
-float& Matrix:: operator()(int r,int c)
+float& Matrix:: operator()(size_t r,size_t c)
 {
     //判断浮点数是否在目标矩阵中
     if(r >= rows || c >= colums){
-        std::cerr <<"超出矩阵范围，查找失败" <<std::endl;
+       throw std::out_of_range("矩阵索引超出范围");
     }
     return elements[r*colums+c];
 }
 
 //重构const版本的()
-const float& Matrix::operator()(int r,int c) const{
+const float& Matrix::operator()(size_t r,size_t c) const{
     //判断浮点数是否在目标矩阵中
     if(r >= rows || c >= colums){
-        std::cerr <<"超出矩阵范围，查找失败" <<std::endl;
+        throw std::out_of_range("矩阵索引超出范围");
     }
     return elements[r*colums+c];
 }
@@ -82,12 +99,23 @@ Matrix Matrix::operator*(const Matrix& other)
     Matrix result(rows,other.colums);
 
     //O(n^3)复杂度实现,还需要重构()来进行矩阵索引,other是const类型，因此()需要重构两次
-    for(int i = 0;i < rows;++i){
-        for(int j = 0;j < other.colums;++j){
-            for(int k = 0;k < colums;++k){
-                result(i,j)=(*this)(i,k) * other(k,j);
+    for(size_t i = 0;i < rows;++i){
+        for(size_t j = 0;j < other.colums;++j){
+            for(size_t k = 0;k < colums;++k){
+                result(i,j)=result(i,j)+(*this)(i,k) * other(k,j);
             }
         }
     }
     return result;
 }
+
+Matrix RELU(Matrix& target){    
+    for (size_t i = 0; i < target.get_colums()*target.get_rows(); ++i)
+    {
+        if(target.elements[i] < 0){
+        target.elements[i] = 0;
+        }
+    }
+    return target;
+}
+
---
+++ part1/test.cpp
@@ -0,0 +1,34 @@
+//这个文件用于测试part1内容
+
+#include "Matrix.h"
+#include <iostream>
+
+int main(){
+    Matrix A(2,3);
+    A(0, 0) =-1.0f; A(0, 1) = 2.0f; A(0, 2) =-3.0f;
+    A(1, 0) = 4.0f; A(1, 1) =-5.0f; A(1, 2) = 6.0f;
+    Matrix B(2,3);
+    B(0, 0) = 1.0f; B(0, 1) = 2.0f; B(0, 2) = 3.0f;
+    B(1, 0) = 4.0f; B(1, 1) = 5.0f; B(1, 2) = 6.0f;
+    Matrix C(3, 2);
+    C(0, 0) = 7.0f; C(0, 1) = 8.0f;
+    C(1, 0) = 9.0f; C(1, 1) = 10.0f;
+    C(2, 0) = 11.0f; C(2, 1) = 12.0f;
+    Matrix D(2,3);
+    Matrix E(2,2);
+    //正常打印  
+    std::cout <<"A:" <<std::endl ; 
+    A.print();
+    //RELU函数
+    RELU(A);
+    std::cout <<"RELU A:" << std::endl;
+    A.print();
+   //矩阵加法
+    D = A + B;
+    std::cout <<"D:" <<std::endl ;
+    D.print();
+    //矩阵乘法
+    E = B * C;
+    std::cout <<"E:" <<std::endl ;
+    E.print();
+}
